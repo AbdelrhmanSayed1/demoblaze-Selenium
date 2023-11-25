b573cc34baca6cf4d32a7c23e4b9cd39fe6e5abd
AbdelrhmanSayed1 <abdelrhmansayed267@gmail.com>
2023-11-25 23:46:15 +0200

    Page Base

---
+++ src/main/java/Pages/PageBase.java
@@ -0,0 +1,13 @@
+package Pages;
+import com.shaft.driver.SHAFT;
+
+public class PageBase {
+    protected SHAFT.GUI.WebDriver driver;
+
+    public PageBase(SHAFT.GUI.WebDriver driver) {
+
+        this.driver = driver;
+
+
+    }
+}
